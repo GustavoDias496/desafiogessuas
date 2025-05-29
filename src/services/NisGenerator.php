0a8c99a53a14a1b9808ab687ec80b504439a5011
GustavoDias496 <gustavodias4966abcd@gmail.com>
2025-05-29 00:35:22 -0300

    feat: service para gerar um numero aleatorio do nis

---
+++ src/services/NisGenerator.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Gustavodias\Desafiogessuas\services;
+
+class NisGenerator {
+    public static function generate(): string {
+        return str_pad(strval(random_int(1,99999999999)), 11, '0', STR_PAD_LEFT);
+    }
+}
