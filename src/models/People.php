c8a233f8882e514efae6a814ceae286678f222da
GustavoDias496 <gustavodias4966abcd@gmail.com>
2025-05-28 23:26:34 -0300

    feat: criando classe para pessoa

---
+++ src/models/People.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Gustavodias\Desafiogessuas\models;
+
+class People {
+    private int $id;
+    private string $name;
+    private string $nis;
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function setId(int $id): void {
+        $this->id = $id;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function setName(int $name): void {
+        $this->name = $name;
+    }
+
+    public function getNis(): string {
+        return $this->nis;
+    }
+
+    public function setNis(string $nis): void {
+        $this->nis = $nis;
+    }
+
+}
