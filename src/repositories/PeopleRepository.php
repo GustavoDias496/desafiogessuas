3bbfc368258c70437e355c026bbc697242ac7cb8
GustavoDias496 <gustavodias4966abcd@gmail.com>
2025-05-29 00:29:25 -0300

    feat: criando repository para a classe people

---
+++ src/repositories/PeopleRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Gustavodias\Desafiogessuas\repositories;
+
+use Gustavodias\Desafiogessuas\config\Database;
+use Gustavodias\Desafiogessuas\models\People;
+
+class PeopleRepository {
+    private $conn;
+
+    public function __construct() {
+        $this->conn = Database::getInstance();
+    }
+
+    public function registerPeople(People $person): void {
+        $stmt = $this->conn->prepare("INSERT INTO people (name, nis) VALUES (:name, :nis)");
+        $stmt->execute([
+            'name' => $person->getName(),
+            'nis' => $person->getNis()
+        ]);
+    }
+
+    public function findByNis(string $nis): ?People {
+        $stmt = $this->conn->prepare("SELECT * FROM people WHERE nis = :nis");
+        $stmt->execute(['nis' => $nis]);
+        $data = $stmt->fetch();
+
+        if(!$data) return null;
+
+        $person = new People();
+        $person->setName($data['name']);
+        $person->setNis($data['nis']);
+        return $person;
+    }
+}
