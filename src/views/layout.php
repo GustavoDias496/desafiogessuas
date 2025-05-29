b5a6c52254b5d3c9b63d7557a10dbf73a924cd92
GustavoDias496 <gustavodias4966abcd@gmail.com>
2025-05-29 20:05:38 -0300

    feat: criando as views de layout, resultado e a view para o formulario

---
+++ src/views/layout.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title><?= $data['title'] ?? 'Cadastro de Cidadão' ?></title>
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+
+    <h1><?= $data['title'] ?? 'Cadastro de Cidadão' ?></h1>
+
+    <?php include $view; ?>
+
+</body>
+</html>
